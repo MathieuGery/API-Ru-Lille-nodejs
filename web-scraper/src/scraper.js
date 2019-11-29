9af2ddfd9b9176f71f3200857d206dbddcde6af3
Mathieu <mathieu.gery@epitech.eu>
2019-11-29 11:01:33 +0100

    [+] begining of web scraper

--- web-scraper/src/scraper.js
+++ web-scraper/src/scraper.js
@@ -5,38 +5,40 @@ const url = 'http://www.crous-lille.fr/restaurant/r-u-chatillon-lille-centre/';
 
 axios(url)
   .then(response => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const statsTable = $('ul.slides');
-    console.log(statsTable.text());
-    topPremierLeagueScorers = [];
+	const html = response.data;
+	const $ = cheerio.load(html);
+	const statsTable = $('ul.slides');
+	topPremierLeagueScorers = [];
 
-    statsTable.each(function () {
-      const date = $(this).find('h3').text().replace(/\s\s+/g, ' ').split(/(?=[A-Z])/);
-      const moment_day = $(this).find('h4').text().split(/(?=[A-Z])/).slice(3);
-      const service = $(this).find('.playerCountry').text();
-      const goals = $(this).find('.mainStat').text();
-
-      console.log(moment_day)
-      date.forEach(element => {
-        moments_array = [];
-        moment_day.forEach(element => {
-          console.log(element)
-        })
+	statsTable.each(function () {
+	  const date = $(this).find('h3').text().replace(/\s\s+/g, ' ').split(/(?=[A-Z])/);
+	  const moment_day = $(this).find('h4').text().split(/(?=[A-Z])/).slice(3);
+	  const service = $(this).find('span').text().split(/(?=[A-Z])/).slice(3);
+	  const plats = $(this).find('ul.liste-plats').text();
 
 
-        topPremierLeagueScorers.push({ 
-          date: element,
-          moment: moments_array,
-          service,
-          goals,
-        });
-      })
-      statsTable.each(function () {
-      })
-    });
+	const moments_array = [];
+	  date.forEach(element => {
+		  tmp_arr = []
+		  service.forEach(elem => {
+			tmp_arr.push({moment: elem , content: "content"})
+		})
+		moments_array.push(tmp_arr)
+		})
 
-    console.log(topPremierLeagueScorers);
+	//parse each days
+	date.forEach(element => {
+		  topPremierLeagueScorers.push({
+		  date: element,
+		  moment: moments_array,
+		  plats,
+		});
+	  })
+	  statsTable.each(function () {
+	  })
+	});
+
+	//console.log(topPremierLeagueScorers);
   })
   .catch(console.error);
 
